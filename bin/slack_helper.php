f7eb2dd9552e2a41df6db9f28186b8440946ad8e
ataylorme <andrew@ataylor.me>
2017-07-18 13:11:00 -0500

    Use different Slack hook URL for the demo

--- bin/slack_helper.php
+++ bin/slack_helper.php
@@ -41,39 +41,12 @@ function _slack_tell( $message, $slack_channel_name, $slack_user_name, $slack_ic
 }
 
 /**
- * Get secrets from secrets file.
- *
- * @param array $requiredKeys List of keys in secrets file that must exist.
- */
-function _get_secrets( $requiredKeys, $defaults ) {
-  if (getenv('SLACK_URL')) {
-    return array('slack_url' => getenv('SLACK_URL'));
-  }
-
-	$secretsFile = $_SERVER['HOME'] . '/files/private/secrets.json';
-	if ( ! file_exists( $secretsFile ) ) {
-		die( 'No secrets file found. Aborting!' );
-	}
-	$secretsContents = file_get_contents( $secretsFile );
-	$secrets         = json_decode( $secretsContents, 1 );
-	if ( $secrets == false ) {
-		die( 'Could not parse json in secrets file. Aborting!' );
-	}
-	$secrets += $defaults;
-	$missing = array_diff( $requiredKeys, array_keys( $secrets ) );
-	if ( ! empty( $missing ) ) {
-		die( 'Missing required keys in json secrets file: ' . implode( ',', $missing ) . '. Aborting!' );
-	}
-
-	return $secrets;
-}
-
-/**
  * Send a notification to slack
  */
 function _slack_notification( $channel, $username, $text, $icon_url, $attachment = false ) {
   $defaults = array();
-	$secrets  = _get_secrets( array( 'slack_url' ), $defaults );
+	$circle_branch = getenv('CIRCLE_BRANCH');
+  $slack_url = ('demo' === $circle_branch) ? getenv('SLACK_HOOK_DEMO_URL') : getenv('SLACK_HOOK_URL');
 
 	$post = array(
 		'username' => $username,
@@ -90,7 +63,7 @@ function _slack_notification( $channel, $username, $text, $icon_url, $attachment
 
 	$payload = json_encode( $post );
 	$ch      = curl_init();
-	curl_setopt( $ch, CURLOPT_URL, $secrets['slack_url'] );
+	curl_setopt( $ch, CURLOPT_URL, $slack_url );
 	curl_setopt( $ch, CURLOPT_POST, 1 );
 	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
 	curl_setopt( $ch, CURLOPT_TIMEOUT, 5 );
